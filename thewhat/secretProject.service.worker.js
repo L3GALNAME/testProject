845cf8231a1e8c006709627249357eb3b545835c
L3GALNAME <supak00pa06@gmail.com>
2025-01-02 13:38:58 -0500

    Borders!!
    Borders work and so does all the resize animations :D

--- thewhat/secretProject.service.worker.js
+++ thewhat/secretProject.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1735600160|4071181054';
+const CACHE_VERSION = '1735843074|62803964228';
 /** @type {string} */
 const CACHE_PREFIX = 'Secret BB Projec-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
