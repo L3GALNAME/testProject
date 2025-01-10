41a7466435d7e65e04540a079aa626720a113a74
L3GALNAME <supak00pa06@gmail.com>
2025-01-10 17:20:58 -0500

    Secret Project v1 !
    Added in some photos and descriptions

--- thewhat/secretProject.service.worker.js
+++ thewhat/secretProject.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1735969396|126629540826';
+const CACHE_VERSION = '1736547554|1274777911';
 /** @type {string} */
 const CACHE_PREFIX = 'Secret BB Projec-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
